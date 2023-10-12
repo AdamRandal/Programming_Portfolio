f589e7d17c6479a521a71186075f4e82a73b9aa1
AdamRandal <146326714+AdamRandal@users.noreply.github.com>
2023-10-12 09:54:42 +0100

    main.py

--- main.py
+++ main.py
@@ -16,3 +16,9 @@ Result = round(NoRunsScor / CompInni,2)
 print(Result)
 
 #5
+Nums = [113,175,12]
+for i in range (3):
+        Div = (Nums[i]//24)
+        Mod = (Nums[i]%24)
+        print(Nums[i],"students will create",Div,"full groups with",Mod,"students left over.")
+
