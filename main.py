1025be07f78ce0e097830f7bdc106f9db5636649
AdamRandal <146326714+AdamRandal@users.noreply.github.com>
2023-10-12 10:04:47 +0100

    main.py

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 # 1
-print ("Hello World")
+print("1.")
+print("Hello World")
+print()
 
 #2
-print ("Hello, Adam R")
+print("2.")
+print("Hello, Adam R")
+print()
 
 #3
+print("3.")
 Celsius = 38.4
 Fahrenheit = round((Celsius * 9/5) + 32, 2)
 print(Celsius,"°C =",Fahrenheit,"°F")
+print()
 
 #4
+print("4.")
 NoRunsScor = 48426
 CompInni = 1014 - 162
 Result = round(NoRunsScor / CompInni,2)
 print(Result)
+print()
 
 #5
+print("5.")
 Nums = [113,175,12]
 for i in range (3):
         Div = (Nums[i]//24)
